c1cef2b1f8c90bb30beaf34757dbf72b818dce31
Hasib <gudhulibela031@gmail.com>
2025-10-17 01:40:45 +0600

    Add files via upload

---
+++ using fputs file.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    FILE *gaypartho;
+    gaypartho=fopen("gay.txt","w");
+    if(gaypartho==NULL)
+    {
+        printf("File not found ");
+    }
+    else
+        {
+             char name[50];
+    printf("Enter the name : ");
+    gets(name);
+    fprintf(gaypartho,"The entered Name\n");
+    fputs(name,gaypartho);
+    fclose(gaypartho);
+
+    }
+
+}
